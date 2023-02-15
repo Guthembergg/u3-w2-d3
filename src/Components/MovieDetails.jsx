7f101fff18925feeed6a3eaf599b0cc75da5b69c
Karim Guettech <karimguettech@gmail.com>
2023-02-15 15:43:07 +0100

    end

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,6 +1,7 @@
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Row, Col } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import CommentArea from "./CommentArea";
 
 const MovieDetails = (props) => {
   const [movie, setMovie] = useState(null);
@@ -34,48 +35,61 @@ const MovieDetails = (props) => {
     <>
       {movie && (
         <>
-          <div className="justify-content-center ">
-            <p
-              className="text-center"
-              style={{ fontWeight: "700", color: "white" }}
+          <Row className="mt-5 d-flex align-items-center text-center flex-column w-100">
+            <Col
+              xs={7}
+              className="mt-5 d-flex align-items-center text-center flex-column"
             >
-              {movie.Title}
-            </p>
-          </div>
-          <div>
-            <p
-              className="text-center"
-              style={{ fontWeight: "700", color: "white" }}
-            >
-              {movie.Actors}
-            </p>
-            <p
-              className="text-center"
-              style={{ fontWeight: "700", color: "white" }}
-            >
-              {movie.Plot}
-            </p>
-            <div className="d-flex justify-content-between">
-              <p
-                className="ml-5"
-                style={{ color: "orange", fontWeight: "700" }}
-              >
-                {movie.imdbRating}{" "}
-              </p>
-              <p className="" style={{ color: "blue", fontWeight: "700" }}>
-                {movie.Released}{" "}
-              </p>
-              <p className="" style={{ color: "grey", fontWeight: "700" }}>
-                {movie.Runtime}{" "}
-              </p>
-              <p
-                className=" mr-5"
-                style={{ color: "green", fontWeight: "700" }}
-              >
-                {movie.Rated}{" "}
-              </p>
-            </div>
-          </div>{" "}
+              <div className="justify-content-center ">
+                <h2
+                  className="text-center"
+                  style={{ fontWeight: "700", color: "white" }}
+                >
+                  {movie.Title}
+                </h2>
+              </div>
+              <div>
+                <p
+                  className="text-center"
+                  style={{ fontWeight: "700", color: "grey" }}
+                >
+                  {movie.Actors}
+                </p>
+                <h3
+                  className="text-center "
+                  style={{ fontWeight: "400", color: "white" }}
+                >
+                  {movie.Plot}
+                </h3>
+                <div className="d-flex justify-content-between mt-5">
+                  <p
+                    className="ml-5"
+                    style={{ color: "orange", fontWeight: "700" }}
+                  >
+                    {movie.imdbRating}{" "}
+                  </p>
+                  <p style={{ color: "white", fontWeight: "700" }}>
+                    {movie.Released}{" "}
+                  </p>
+                  <p className="" style={{ color: "grey", fontWeight: "700" }}>
+                    {movie.Runtime}{" "}
+                  </p>
+                  <p
+                    className=" mr-5"
+                    style={{ color: "green", fontWeight: "700" }}
+                  >
+                    {movie.Rated}{" "}
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row xs={7} className="d-flex mt-5 w-100">
+            <Col className="d-flex align-items-center text-center flex-column">
+              <h2>Comment Area</h2>
+              <CommentArea asin={movieParam.movieId} />
+            </Col>
+          </Row>
         </>
       )}
     </>
---
+++ src/Components/SingleComment.jsx
@@ -0,0 +1,44 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/" + asin,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U0ZjM3ZmEyNDc4ZDAwMTNhMDU3ZmMiLCJpYXQiOjE2NzU5NDg5MjcsImV4cCI6MTY3NzE1ODUyN30.KLbQdFihLwwaAc5BHQ0L8HaPauH90qhugo64glVZLaw",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Comment was deleted successfully!");
+      } else {
+        alert("Error - comment was NOT deleted!");
+      }
+    } catch (error) {
+      alert("Error - comment was NOT deleted!");
+    }
+  };
+
+  return (
+    <ListGroup.Item className="mt-3 mb-5">
+      <div className="d-flex justify-content-between align-items-center">
+        {comment.comment}
+        <div className="d-flex align-items-center">
+          <Button
+            variant="danger"
+            className="ml-2"
+            onClick={() => deleteComment(comment._id)}
+          >
+            Delete
+          </Button>
+        </div>
+      </div>
+    </ListGroup.Item>
+  );
+};
+
+export default SingleComment;
